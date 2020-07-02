bc031ef4099c8d02b0eafca32d030cf729e67406
Kishore Newton <kishorenke@gmail.com>
2020-07-03 00:00:38 +0530

    changed the status back to 200

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -5,7 +5,7 @@ it('Responds with deatils about the current user', async() => {
     
     const cookie = await global.signin()
 
-    const response = await request(app).get('/api/users/currentuser').set('Cookie', cookie).send().expect(400)
+    const response = await request(app).get('/api/users/currentuser').set('Cookie', cookie).send().expect(200)
 
     expect(response.body.currentUser.email).toEqual('test@test.com')
 })
